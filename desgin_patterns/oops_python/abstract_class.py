631eb671b05f47c7642370679091c5418c4c49be
viswanath27 <code.vissu@gmail.com>
2022-12-10 09:43:53 +0530

    update
    update

---
+++ desgin_patterns/oops_python/abstract_class.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class AbstractClassExample(ABC):
+
+    def __init__(self, value):
+        self.value = value
+        super().__init__()
+    
+    @abstractmethod
+    def do_something(self):
+        pass
+
+class DoAdd42(AbstractClassExample):
+    def do_something(self):
+        return self.value + 42
+
+
+x = DoAdd42(10)
+print(x.do_something())
